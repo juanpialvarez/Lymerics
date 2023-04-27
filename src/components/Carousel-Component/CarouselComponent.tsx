6b961935a69165023e95ab979221d6d129d73792
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-04-26 21:10:56 -0600

    Add fix responsive carousel

--- src/components/Carousel-Component/CarouselComponent.tsx
+++ src/components/Carousel-Component/CarouselComponent.tsx
@@ -5,7 +5,8 @@ import {
   ButtonBack,
   ButtonNext,
 } from "pure-react-carousel";
-import React from "react";
+import { Icon } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
 import { Story } from "../../story";
 import StoryCard from "../Story-Card-Component/SotryCard";
 import "pure-react-carousel/dist/react-carousel.es.css";
@@ -15,19 +16,34 @@ interface Props {
 }
 
 const CarouselComponent: React.FC<Props> = ({ data }: Props) => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    function handleDisplay() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleDisplay);
+  });
+
   return (
     <CarouselProvider
-      naturalSlideWidth={1.5}
+      naturalSlideWidth={1}
       naturalSlideHeight={1}
       totalSlides={data.length}
-      visibleSlides={3}
+      visibleSlides={
+        windowWidth > 950 ? 5 : windowWidth <= 950 && windowWidth > 750 ? 4 : 3
+      }
       className='carousel'
     >
       <div className='carousel__buttons'>
-        <ButtonBack className='ui button btn-back'>Back</ButtonBack>
-        <ButtonNext className='ui button btn-forward'>Next</ButtonNext>
+        <ButtonBack className='ui button btn-back'>
+          <Icon name='chevron left' />
+        </ButtonBack>
+        <ButtonNext className='ui button btn-forward'>
+          <Icon name='chevron right' />
+        </ButtonNext>
       </div>
-      <Slider>
+      <Slider className='slider'>
         {data.map((story, index) => (
           <Slide index={index}>
             <StoryCard story={story} />
